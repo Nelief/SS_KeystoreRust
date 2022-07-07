178482fbbbda23ac290ea4e91e6b5fbef407ccbc
Nelief <madima.mauriade@gmail.com>
2022-07-07 19:27:15 +0200

    blallo

--- src/function.rs
+++ src/function.rs
@@ -1,6 +1,7 @@
-use std::fs::OpenOptions;
-use std::io::{Write, Read};
+use std::fs::{OpenOptions,remove_file};
+use std::io::{Write, Read,BufReader,BufRead,self};
 use std::str; 
+use std::collections::HashMap;
 
 use pwhash::bcrypt::{verify,hash};
 use rand::{RngCore, SeedableRng};
@@ -14,6 +15,7 @@ use block_modes::{BlockMode,Ecb};
 use std::fs::{File};
 
 use std::os::windows::fs::OpenOptionsExt;
+
 
 type Aes128Ecb = Ecb<Aes128, Pkcs7>;
 
@@ -39,8 +41,8 @@ pub fn verify_password_hash(password : &str, hash_path : &str) -> bool{
 /// 
 ///genera una chiave di encription a partire dalla stringa data ( usata come seed) sfruttando il generatore di numeri casuali della libreria AesRng
 pub fn create_crypto_key(password : &str) -> [u8;16]{
-    let seed = &password.parse::<u64>().unwrap();
-    let mut rng = AesRng::seed_from_u64(*seed);
+    let seed = &password.parse::<u64>().unwrap(); 
+    let mut rng = AesRng::seed_from_u64(*seed); 
     let mut bytes = [0u8;16];
     rng.fill_bytes(&mut bytes);
     bytes
@@ -106,16 +108,14 @@ pub fn print_secret(path : &str, secret : String, key : [u8;16]){
     .open(path)
     .unwrap();
 
-    writeln!(output,"{}",process_encrypted_secret(secret, key)).unwrap();
-
-    print!("Segreto inserito nel portachiavi!");
+    writeln!(output,"{}",process_encrypted_secret(secret, key)).unwrap();  
 }
 
 ///Input : String : master password per l' accesso al keystore , &str : Path al file dei segreti , &str : Path al file di configurazione
 /// 
 ///la fn genera la hash della password ricevuta chiamando bycrypt.hash() e procede a inizializzare e creare 2 file nascosti (FILE_ATTRIBUTE_HIDDEN di windows), termina scrivendo la hash sul file di configurazione 
 pub fn make_keychain_cofing(password : String, file_path: &str, config_path: &str){
-    println!("creazione nuovo keychain");
+    println!("creazione nuovo keystore");
     //estrarre variabili arg
     let password = password.as_str();
     let pw_hash = hash(password).unwrap();
@@ -138,4 +138,153 @@ pub fn make_keychain_cofing(password : String, file_path: &str, config_path: &st
     .open(file_path)
     .unwrap();
 
+}
+
+///stampa e richiede la password master da stdin
+pub fn get_password () -> String{
+    //richiesta pw 
+    print!("Inserire password master >> ");
+    let _ = io::stdout().flush();
+
+    //password from stdinput
+    let in_reader = BufReader::new(io::stdin());
+    in_reader.lines().next().unwrap().unwrap()
+}
+
+///Estrae il segreto da "line", lo decritta, ed inserisce nella hashmap
+pub fn to_map(secret_map: & mut HashMap<String, String>, line : String, key : [u8;16]){
+    let mut u8buf = hex::decode(line).unwrap(); 
+    let iv = hex!("");
+
+    let cipher = Aes128Ecb::new_from_slices(&key, &iv).unwrap();
+    
+    let decrypted_text = cipher.decrypt(&mut u8buf).unwrap();
+    let text = str::from_utf8(decrypted_text).unwrap().to_owned();
+
+    let v : Vec<&str> = text.split('|').collect();
+    let key = v[0].clone().to_owned();
+    let secret = v[1].clone().to_owned();
+
+    secret_map.insert(key.clone().to_owned(),secret.clone().to_owned());
+}
+
+///rigenera un nuovo keystore per l' update 
+pub fn reset_secret(path : &str){
+    remove_file(path).unwrap();
+
+    _ = OpenOptions::new()
+    .create(true)
+    .write(true)
+    .open(path)
+    .unwrap();
+}
+
+///processa path e password per decriptare il keystore e prepara la hash per la ricerca 
+pub fn search_key(query_key : String, path : &str, password : &str){
+    let  secret_map = generate_hashmap(password, path);
+    
+    if secret_map.contains_key(&query_key) {
+        let (hkey,hval) = secret_map.get_key_value(&query_key).unwrap();
+        print!("chiave : {} | Segreto : {}" ,hkey,hval);
+    } else {
+        println!("Chiave non trovata!");
+    }  
+}
+
+
+///cancella la keypair indicata da query_key se presente ed aggiorna il keystore
+pub fn delete_keypair(password : &str, query_key : String, path : &str){
+
+    let mut secret_map =  generate_hashmap(password, path);
+
+    if secret_map.contains_key(&query_key) {
+        
+        secret_map.remove(&query_key);
+
+        let mut secret_vec = Vec::new();                
+        for k in secret_map.keys(){
+            let (key,value) = secret_map.get_key_value(k).unwrap();
+            secret_vec.push(make_secret_line(key.to_string(), value.to_string()));
+        }    
+
+        let key = create_crypto_key(password);
+
+        let mut i = 0;
+        for s in secret_vec{
+            if i == 0{
+                reset_secret(path); 
+                i=i+1;
+            } 
+            print_secret(path, s, key)
+            
+        }
+       
+       print!("Chiave rimossa con successo!");
+    } else {
+        println!("Chiave non trovata!")
+    }
+}
+
+///genera una hashmap a partire dal file keystore
+pub fn generate_hashmap(password : &str, path : &str) -> HashMap<String,String>{
+    let key = create_crypto_key(password);
+    let f = File::open(path).unwrap();
+    let reader = BufReader::new(f);
+
+    let mut secret_map : HashMap<String,String> = HashMap::new();
+
+    for line in reader.lines(){
+        match line{
+            Ok(line) => {
+                if line.len()>0 {
+                    to_map(&mut secret_map, line, key);   
+                }
+            }     
+            Err(e) => println!("Errorr : {}",e),
+        }        
+    }
+    secret_map
+}
+
+///aggiunge un segreto univoco al keystore cifrandolo con la password 
+pub fn add_secret(password: &str, key : String , secret : String,path : &str){
+    let ckey = create_crypto_key(password);
+    let secret_name = key;
+    let secret_value = secret;
+    if secret_name.len() <=20 && secret_value.len() <=100{
+        let secret = make_secret_line(secret_name, secret_value);
+        print_secret(path, secret, ckey);
+        print!("Segreto inserito nel portachiavi!");
+    }else{
+        println!("Error: chiave o segreto troppo lunghi (20/100 char limit)!");
+    }
+}
+
+///verifica che la chiave non sia gia presente nel keystore
+pub fn verify_key(key : &String, path : &str, password : &str) -> bool{
+    let  secret_map = generate_hashmap(password, path);
+    
+    if secret_map.contains_key(key) {
+        true
+    } else {
+        false
+    }  
+}
+
+///stampa a video la lista dei segreti nel keystore
+pub fn list_secrets(password : &str, path : &str){
+    let key = create_crypto_key(password);
+    let f = File::open(path).unwrap();
+    let reader = BufReader::new(f);
+
+    for line in reader.lines(){
+        match line{
+            Ok(line) => {
+                if line.len()>0 {
+                    process_secret(line, key);
+                }
+            }     
+            Err(e) => println!("Errorr : {}",e),
+        } 
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod function;
-use function::{verify_password_hash,create_crypto_key,process_secret,make_secret_line,print_secret,make_keychain_cofing};
+use function::*;
 
 use clap::{Parser,ArgEnum};
-
-use std::fs::{File,remove_file};
-use std::io::{BufReader, BufRead, Error};
+use std::fs::remove_file;
+use std::io::Error;
 use std::path::Path;
+use std::str;
 
 //Enum della lista dei comandi legali sul sistema 
 #[derive(Debug,PartialEq,Clone,ArgEnum)]
@@ -14,6 +14,8 @@ enum Cmd{
     Add,
     New,
     Reset,
+    Search,
+    Delete
 }
 
 //Struct di parsing per gli input CLI
@@ -21,85 +23,109 @@ enum Cmd{
 #[derive(Parser, Debug)]
 #[clap(disable_version_flag(true))]
 struct Args {
-    #[clap(long,id="cmd",short = 'c',value_parser,arg_enum,hide_possible_values(true),help ="Specifica il comando da eseguire sul keystore\n\x1b[92mnew\x1b[0m    : crea nuovo keystore [new <password>]\n\x1b[92madd\x1b[0m    : aggiunge un nuovo segreto [add <password> <chiave>  \"<segreto1> <segreto2>...\" ]\n\x1b[92mlist\x1b[0m   : mostra segreti [list <password>]\n\x1b[92mreset\x1b[0m  : resetta keystore [reset <password>] NON REVERSIBILE\n"                                  )]
+    #[clap(long,id="cmd",short = 'c',value_parser,arg_enum,hide_possible_values(true),help ="Specifica il comando da eseguire sul keystore\n\x1b[92mnew\x1b[0m    : crea nuovo keystore [new <password>]\n\x1b[92madd\x1b[0m    : aggiunge un nuovo segreto [add <password> <chiave>  \"<segreto1> <segreto2>...\" ]\n\x1b[92mlist\x1b[0m   : mostra segreti [list <password>]\n\x1b[92mreset\x1b[0m  : resetta keystore [reset <password>] NON REVERSIBILE\n\x1b[92msearch\x1b[0m    : visualizza il segreto indicato dalla chiave [search <chiave>]\n\x1b[92mdelete\x1b[0m    : elimina il segreto indicato dalla chiave [delete <chiave>]\n"                                  )]
     cmd : Cmd,
-
-    #[clap(long,short = 'p',value_parser,help="Password di accesso al keystore")]
-    pw : String,
 
     #[clap(long,short = 'k',value_parser,required_if_eq("cmd","add"),default_value="",help =r#"ID/riferimento per il segreto[necessario per comando "Add"]"#, hide_default_value(true))]
     key : String,
     
     #[clap(long,short = 's',value_parser,required_if_eq("cmd","add"),default_value="",help =r#"Descrizione Segreto[necessario per comando "Add"]"#,hide_default_value(true))]
     secret : String, 
+
+    #[clap(long,short = 'q',value_parser,required_if_eq("cmd","search"),default_value="",help =r#"Chiave di ricerca [necessaria per comando "Search"]"#,hide_default_value(true))]
+    query_key : String, 
 }
 
 //nomi dei file nascosti usati dal sistema 
-static PATH : &str = "file.txt";
+static PATH : &str = "keystore.txt";
 static CPATH : &str = "cfg.txt";
 
 fn main() -> Result<(), Error> {
     let args = Args::parse();
-    
+
+    let input_password = get_password();
+   
     match args.cmd {
         Cmd::New => {
             if Path::new(PATH).exists() {
-                println!("keystore already exists, please reset before configuring a new one!");
+                println!("keystore già esistente!");
             }else{
-                let password = args.pw;
-                if  password.len() > 7 {
-                    make_keychain_cofing(password, PATH, CPATH);
+                if  input_password.len() > 7 {
+                    make_keychain_cofing(input_password, PATH, CPATH);
                 } else {
-                    println!("Error: password is too short ( 8 char min)!");
+                    println!("Error: password troppo corta ( 8 char min)!");
                 }
             }
         }
         Cmd::List => {
-            let password = args.pw.as_str();
-            if verify_password_hash(password,CPATH){
-                let key = create_crypto_key(password);
-                println!("printing...");
-                let f = File::open(PATH)?;
-                let reader = BufReader::new(f);
-
-                for line in reader.lines(){
-                    match line{
-                        Ok(line) => {
-                            if line.len()>0 {
-                                process_secret(line, key);
-                            }
-                        }     
-                        Err(e) => println!("Errorr : {}",e),
-                    }        
-                }  
-            } else{
-                println!("Wrong password");
+            if Path::new(PATH).exists() {
+                let password = input_password.as_str();
+                if verify_password_hash(password,CPATH){
+                    list_secrets(password, PATH);
+                } else{
+                    println!("password errata");
+                }
+            }else{
+                println!(r#"keystore inesistente (inizializzare con "new")"#);
             }
         }
         Cmd::Add => {
-            let password = args.pw.as_str();
-            if verify_password_hash(password,CPATH){
-                let key = create_crypto_key(password);
-                let secret_name = args.key;
-                let secret_value = args.secret;
-                if secret_name.len() <=20 && secret_value.len() <=100{
-                    let secret = make_secret_line(secret_name, secret_value);
-                    print_secret(PATH, secret, key);
-                }else{
-                    println!("Error: key or secret are too long (20/100 char limit)!");
+            if Path::new(PATH).exists() {
+                let password = input_password.as_str();
+                if verify_password_hash(password,CPATH){
+                    if !verify_key(&args.key, PATH, password){
+                        add_secret(password, args.key, args.secret, PATH);
+                    }else{
+                        println!("chiave già presente!");
+                    }
+                } else{
+                    println!("password errata");
                 }
-            } else{
-                println!("Wrong password");
+            }else{
+                println!(r#"keystore inesistente (inizializzare con "new")"#);
             }
+            
         }
         Cmd::Reset => {
-            let password = args.pw.as_str();
-            if verify_password_hash(password,CPATH){
-                remove_file(PATH)?;
-                remove_file(CPATH)?;
-                print!("keystore reset complete...");
+            if Path::new(PATH).exists() {
+                    let password = input_password.as_str();
+                if verify_password_hash(password,CPATH){
+                    remove_file(PATH)?;
+                    remove_file(CPATH)?;
+                    print!("keystore cancellato con successo...");
+                }
+            }else{
+                println!(r#"keystore inesistente (inizializzare con "new")"#);
             }
+            
+        }
+        Cmd::Search =>{
+            if Path::new(PATH).exists() {
+                    let password = input_password.as_str();
+                if verify_password_hash(password,CPATH){
+                    search_key(args.query_key, PATH, password);
+                } else{
+                    println!("password errata");
+                }
+            }else{
+                println!(r#"keystore inesistente (inizializzare con "new")"#);
+            }
+            
+        } 
+        Cmd::Delete => {
+            if Path::new(PATH).exists() {
+                    let password = input_password.as_str();
+                if verify_password_hash(password,CPATH){
+                    delete_keypair(password, args.query_key, PATH)
+                } else{
+                    println!("password errata");
+                }
+            }else{
+                println!(r#"keystore inesistente (inizializzare con "new")"#);
+            }
+            
         }
     }
     Ok(())
 }
+
